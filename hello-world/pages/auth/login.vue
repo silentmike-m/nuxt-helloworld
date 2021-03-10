cdbc46ac652dd88eebaa16e94187f6d9b204fd60
silentmike-m <mike@silentmike.pl>
2021-03-10 11:31:05 +0100

    create hello-world

---
+++ hello-world/pages/auth/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Logo inputText="Login Page" />
+    <form @submit.prevent="loginUser()">
+      <input type="text" v-model="userName" />
+      <input type="password" v-model="password" />
+      <button type="submit">LOGIN</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Logo from "~/components/Logo.vue";
+
+export default Vue.extend({
+  components: {
+    Logo,
+  },
+  data(): {
+    userName: String;
+    password: String;
+  } {
+    return {
+      userName: "user name",
+      password: "password",
+    };
+  },
+  methods: {
+    loginUser() {
+      console.log(`userName: ${this.userName}`);
+      console.log(`password: ${this.password}`);
+    },
+  },
+});
+</script>
